6e0dbf5a12e38cc6ed231351be5bc12d6990358c
Tibane 219042721 <219042721@ump.ac.za>
2022-06-14 19:58:52 +0200

    Loan of 7%

---
+++ Loan.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Loan{
+	
+	public static void main(String args[]){
+		float P = 1200, R =7, T=30;
+		
+		float SI = (P * T * R) / 100
+
+		System.out.println("SI is: " +SI)
+	}
+}
